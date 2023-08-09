0762ba9030e79cfab63e8b113b284ab72c892124
kkjayakumar <kkjayakumar@live.com>
2023-08-09 17:58:24 +0530

    Update sqs.tf

--- sqs.tf
+++ sqs.tf
@@ -1,3 +1,13 @@
-resource "aws_sqs_queue" "my_queue1" {
-  name = "my-queue1"
+terraform {
+  required_providers {
+    aws = "~> 3.79.0"
+  }
+
+  provider "aws" {
+    region = "us-east-1"
+  }
+
+  resource "aws_sqs_queue" "my_queue1" {
+    queue_name = "my-queue1"
+  }
 }
