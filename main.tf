60895e06217af252fc6ec395b2ec616e6afbed91
kkjayakumar <kkjayakumar@live.com>
2023-08-10 18:56:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ terraform {
 # Provider Block
 provider "aws" {
   region  = "ap-south-1"
+  access_key_id = var.aws_access_key_id
+  secret_access_key = var.aws_secret_access_key
   profile = "default"
 }
 
